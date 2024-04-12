22c2f94b5f9da896470ce25cfeaa7e9c5ddd5ca8
extradosages <extradosages.001@gmail.com>
2024-04-12 17:04:47 -0400

    fix: revert to using `enum` for better inference

--- src/compiler/ast/stronglyTyped/common/parsers/nodes.ts
+++ src/compiler/ast/stronglyTyped/common/parsers/nodes.ts
@@ -1,14 +1,15 @@
+import z from "zod";
 import {
     largeTypeKindParser,
-    nodeKindParser,
     smallTypeKindParser,
     termKindParser,
 } from "../../../defs";
+import { strongNodeKinds } from "../enumeration";
 
-export const strongNodeKindParser = nodeKindParser;
+export const strongNodeKindParser = z.enum(strongNodeKinds);
 
 export const strongTermNodeKindParser = termKindParser;
 
-export const strongSmallTypeNodeKIndParser = smallTypeKindParser;
+export const strongSmallTypeNodeKindParser = smallTypeKindParser;
 
 export const strongLargeTypeNodeKindParser = largeTypeKindParser;
--- src/compiler/ast/weaklyTyped/types.ts
+++ src/compiler/ast/weaklyTyped/types.ts
@@ -1,15 +1,15 @@
-import { parserBundle } from "./parserBundles";
-import { WeakNodeKind } from "./common";
+import { WeakEdgeKind, WeakNodeKind } from "./common";
 import { WeakDenormalizedData } from "./denormalized";
 import {
     WeakEdgeMap_SourceKindT_EdgeKindT_,
     WeakNormalizedData,
-    WeakNormalizedNode,
 } from "./normalized";
+import { parserBundle } from "./parserBundles";
 
 export type WeakAstParsers<
     Kind extends WeakNodeKind,
     DData extends WeakDenormalizedData,
     NData extends WeakNormalizedData,
+    EdgeKind extends WeakEdgeKind,
     NEdges extends WeakEdgeMap_SourceKindT_EdgeKindT_,
-> = ReturnType<typeof parserBundle<Kind, DData, NData, NEdges>>;
+> = ReturnType<typeof parserBundle<DData, EdgeKind, Kind, NData, NEdges>>;
