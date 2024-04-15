e2967eb336634a562fcf679615eb38ac3602e5f7
extradosages <extradosages.001@gmail.com>
2024-04-15 19:26:24 -0400

    feat: make resolutions into classes

--- src/compiler/analyze/resolveTermReferences.ts
+++ src/compiler/analyze/resolveTermReferences.ts
@@ -4,108 +4,123 @@ import { NormalizedAst } from "../ast";
 import { ErrorWithContext } from "../util/errorsWithContext";
 import { Scopes } from "./scopes";
 
-export type TermResolution = Record<string, string>;
+export class TermResolution {
+    #lookup: Record<string, string>;
 
-const kernel = (
-    ast: NormalizedAst,
-    scopes: Scopes,
-    scopeTermBindings: Record<string, string[]>,
-    scopeId: string,
-    termReferenceIdentifier: string,
-): string | undefined => {
-    const localTermBindingIds = scopeTermBindings[scopeId];
-    if (localTermBindingIds === undefined) {
-        throw new ErrorWithContext(
-            { scopeId, scopeTermBindings },
-            "Scope not found",
-        );
-    }
-
-    const resolution = localTermBindingIds.find((termBindingId) => {
-        const termBindingIdentifier =
-            ast.node<"termBinding">(termBindingId).data.value;
-        return termBindingIdentifier === termReferenceIdentifier;
-    });
-
-    if (resolution === undefined) {
-        const scopeKind = ast.graph.getNodeAttribute(scopeId, "kind");
-
-        if (scopeKind === "lambdaConstructor") {
-            const nextScopeId = scopes.lookup(scopeId);
-            return kernel(
-                ast,
-                scopes,
-                scopeTermBindings,
-                nextScopeId,
-                termReferenceIdentifier,
+    #kernel(
+        ast: NormalizedAst,
+        scopes: Scopes,
+        scopeTermBindings: Record<string, string[]>,
+        scopeId: string,
+        termReferenceIdentifier: string,
+    ): string | undefined {
+        const localTermBindingIds = scopeTermBindings[scopeId];
+        if (localTermBindingIds === undefined) {
+            throw new ErrorWithContext(
+                { scopeId, scopeTermBindings },
+                "Scope not found",
             );
         }
 
-        if (scopeKind === "library") {
-            return undefined;
+        const resolution = localTermBindingIds.find((termBindingId) => {
+            const termBindingIdentifier =
+                ast.node<"termBinding">(termBindingId).data.value;
+            return termBindingIdentifier === termReferenceIdentifier;
+        });
+
+        if (resolution === undefined) {
+            const scopeKind = ast.graph.getNodeAttribute(scopeId, "kind");
+
+            if (scopeKind === "lambdaConstructor") {
+                const nextScopeId = scopes.lookup(scopeId);
+                return this.#kernel(
+                    ast,
+                    scopes,
+                    scopeTermBindings,
+                    nextScopeId,
+                    termReferenceIdentifier,
+                );
+            }
+
+            if (scopeKind === "library") {
+                return undefined;
+            }
+
+            throw new ErrorWithContext(
+                { scopeId, scopeKind },
+                'Scope kind is not `"library"` or `"lambdaConstructor"`',
+            );
         }
 
-        throw new ErrorWithContext(
-            { scopeId, scopeKind },
-            'Scope kind is not `"library"` or `"lambdaConstructor"`',
-        );
+        return resolution;
     }
 
-    return resolution;
-};
+    constructor(ast: NormalizedAst) {
+        const scopes = new Scopes(["lambdaConstructor"], ast);
 
-export const resolveTermReferences = (ast: NormalizedAst): TermResolution => {
-    const scopes = new Scopes(["lambdaConstructor"], ast);
-
-    // Associate to each scope a set of term bindings.
-    const termBindingNodeIds = ast.graph.filterNodes(
-        (_, node) => node.kind === "termBinding",
-    );
-
-    const scopeTermBindings = groupBy(termBindingNodeIds, (nodeId) =>
-        scopes.lookup(nodeId),
-    );
-
-    const termReferenceNodeIds = ast.graph.filterNodes(
-        (_, node) => node.kind === "termReference",
-    );
-
-    const resolutions = termReferenceNodeIds.map((nodeId) => {
-        // Look-up the scope id and the identifier for the resolution
-        const scopeId = scopes.lookup(nodeId);
-        const identifier = ast.node<"termReference">(nodeId).data.value;
-
-        const resolution = kernel(
-            ast,
-            scopes,
-            scopeTermBindings,
-            scopeId,
-            identifier,
+        // Associate to each scope a set of term bindings.
+        const termBindingNodeIds = ast.graph.filterNodes(
+            (_, node) => node.kind === "termBinding",
         );
 
-        return [nodeId, resolution] as const;
-    });
-
-    // Separate out the failures and throw if there are any.
-    const failures = resolutions.filter(
-        ([_, resolution]) => resolution === undefined,
-    );
-
-    if (failures.length > 0) {
-        const unresolvableTermReferenceNodes = failures.map(([nodeId]) =>
-            ast.node<"termReference">(nodeId),
+        const scopeTermBindings = groupBy(termBindingNodeIds, (nodeId) =>
+            scopes.lookup(nodeId),
         );
 
-        throw new ErrorWithContext(
-            { unresolvableTermReferenceNodes },
-            "Failed to resolve term references",
+        const termReferenceNodeIds = ast.graph.filterNodes(
+            (_, node) => node.kind === "termReference",
         );
+
+        const resolutions = termReferenceNodeIds.map((nodeId) => {
+            // Look-up the scope id and the identifier for the resolution
+            const scopeId = scopes.lookup(nodeId);
+            const identifier = ast.node<"termReference">(nodeId).data.value;
+
+            const resolution = this.#kernel(
+                ast,
+                scopes,
+                scopeTermBindings,
+                scopeId,
+                identifier,
+            );
+
+            return [nodeId, resolution] as const;
+        });
+
+        // Separate out the failures and throw if there are any.
+        const failures = resolutions.filter(
+            ([_, resolution]) => resolution === undefined,
+        );
+
+        if (failures.length > 0) {
+            const unresolvableTermReferenceNodes = failures.map(([nodeId]) =>
+                ast.node<"termReference">(nodeId),
+            );
+
+            throw new ErrorWithContext(
+                { unresolvableTermReferenceNodes },
+                "Failed to resolve term references",
+            );
+        }
+
+        // Separate out the successes and return a map
+        const successes = resolutions.filter(
+            (pair): pair is [string, string] => pair[1] !== undefined,
+        );
+
+        this.#lookup = fromPairs(successes);
     }
 
-    // Separate out the successes and return a map
-    const successes = resolutions.filter(
-        (pair): pair is [string, string] => pair[1] !== undefined,
-    );
+    lookup(termReferenceNodeId: string): string {
+        const resolution = this.#lookup[termReferenceNodeId];
 
-    return fromPairs(successes);
-};
+        if (resolution === undefined) {
+            throw new ErrorWithContext(
+                { termReferenceNodeId },
+                "Term reference unresolvable",
+            );
+        }
+
+        return resolution;
+    }
+}
--- src/compiler/analyze/resolveTypeReferences.ts
+++ src/compiler/analyze/resolveTypeReferences.ts
@@ -4,108 +4,123 @@ import { NormalizedAst } from "../ast";
 import { ErrorWithContext } from "../util/errorsWithContext";
 import { Scopes } from "./scopes";
 
-type TypeResolution = Record<string, string>;
+export class TypeResolution {
+    #lookup: Record<string, string>;
 
-const kernel = (
-    ast: NormalizedAst,
-    scopes: Scopes,
-    scopeTypeBindings: Record<string, string[]>,
-    scopeId: string,
-    typeReferenceIdentifier: string,
-): string | undefined => {
-    const localTypeBindingIds = scopeTypeBindings[scopeId];
-    if (localTypeBindingIds === undefined) {
-        throw new ErrorWithContext(
-            { scopeId, scopeTypeBindings },
-            "Scope not found",
-        );
-    }
-
-    const resolution = localTypeBindingIds.find((typeBindingId) => {
-        const typeBindingIdentifier =
-            ast.node<"typeBinding">(typeBindingId).data.value;
-        return typeBindingIdentifier === typeReferenceIdentifier;
-    });
-
-    if (resolution === undefined) {
-        const scopeKind = ast.graph.getNodeAttribute(scopeId, "kind");
-
-        if (scopeKind === "genericTypeConstructor") {
-            const nextScopeId = scopes.lookup(scopeId);
-            return kernel(
-                ast,
-                scopes,
-                scopeTypeBindings,
-                nextScopeId,
-                typeReferenceIdentifier,
+    #kernel(
+        ast: NormalizedAst,
+        scopes: Scopes,
+        scopeTypeBindings: Record<string, string[]>,
+        scopeId: string,
+        typeReferenceIdentifier: string,
+    ): string | undefined {
+        const localTypeBindingIds = scopeTypeBindings[scopeId];
+        if (localTypeBindingIds === undefined) {
+            throw new ErrorWithContext(
+                { scopeId, scopeTypeBindings },
+                "Scope not found",
             );
         }
 
-        if (scopeKind === "library") {
-            return undefined;
+        const resolution = localTypeBindingIds.find((typeBindingId) => {
+            const typeBindingIdentifier =
+                ast.node<"typeBinding">(typeBindingId).data.value;
+            return typeBindingIdentifier === typeReferenceIdentifier;
+        });
+
+        if (resolution === undefined) {
+            const scopeKind = ast.graph.getNodeAttribute(scopeId, "kind");
+
+            if (scopeKind === "genericTypeConstructor") {
+                const nextScopeId = scopes.lookup(scopeId);
+                return this.#kernel(
+                    ast,
+                    scopes,
+                    scopeTypeBindings,
+                    nextScopeId,
+                    typeReferenceIdentifier,
+                );
+            }
+
+            if (scopeKind === "library") {
+                return undefined;
+            }
+
+            throw new ErrorWithContext(
+                { scopeId, scopeKind },
+                'Scope kind is not `"library"` or `"genericTypeConstructor"`',
+            );
         }
 
-        throw new ErrorWithContext(
-            { scopeId, scopeKind },
-            'Scope kind is not `"library"` or `"genericTypeConstructor"`',
-        );
+        return resolution;
     }
 
-    return resolution;
-};
+    constructor(ast: NormalizedAst) {
+        const scopes = new Scopes(["genericTypeConstructor"], ast);
 
-export const resolveTypeReferences = (ast: NormalizedAst): TypeResolution => {
-    const scopes = new Scopes(["genericTypeConstructor"], ast);
-
-    // Associate to each scope a set of term bindings.
-    const typeBindingNodeIds = ast.graph.filterNodes(
-        (_, node) => node.kind === "typeBinding",
-    );
-
-    const scopeTypeBindings = groupBy(typeBindingNodeIds, (nodeId) =>
-        scopes.lookup(nodeId),
-    );
-
-    const typeReferenceNodeIds = ast.graph.filterNodes(
-        (_, node) => node.kind === "typeReference",
-    );
-
-    const resolutions = typeReferenceNodeIds.map((nodeId) => {
-        // Look-up the scope id and the identifier for the resolution
-        const scopeId = scopes.lookup(nodeId);
-        const identifier = ast.node<"typeReference">(nodeId).data.value;
-
-        const resolution = kernel(
-            ast,
-            scopes,
-            scopeTypeBindings,
-            scopeId,
-            identifier,
+        // Associate to each scope a set of term bindings.
+        const typeBindingNodeIds = ast.graph.filterNodes(
+            (_, node) => node.kind === "typeBinding",
         );
 
-        return [nodeId, resolution] as const;
-    });
-
-    // Separate out the failures and throw if there are any.
-    const failures = resolutions.filter(
-        ([_, resolution]) => resolution === undefined,
-    );
-
-    if (failures.length > 0) {
-        const unresolvableTypeReferenceNodes = failures.map(([nodeId]) =>
-            ast.node<"typeReference">(nodeId),
+        const scopeTypeBindings = groupBy(typeBindingNodeIds, (nodeId) =>
+            scopes.lookup(nodeId),
         );
 
-        throw new ErrorWithContext(
-            { unresolvableTypeReferenceNodes },
-            "Failed to resolve type references",
+        const typeReferenceNodeIds = ast.graph.filterNodes(
+            (_, node) => node.kind === "typeReference",
         );
+
+        const resolutions = typeReferenceNodeIds.map((nodeId) => {
+            // Look-up the scope id and the identifier for the resolution
+            const scopeId = scopes.lookup(nodeId);
+            const identifier = ast.node<"typeReference">(nodeId).data.value;
+
+            const resolution = this.#kernel(
+                ast,
+                scopes,
+                scopeTypeBindings,
+                scopeId,
+                identifier,
+            );
+
+            return [nodeId, resolution] as const;
+        });
+
+        // Separate out the failures and throw if there are any.
+        const failures = resolutions.filter(
+            ([_, resolution]) => resolution === undefined,
+        );
+
+        if (failures.length > 0) {
+            const unresolvableTypeReferenceNodes = failures.map(([nodeId]) =>
+                ast.node<"typeReference">(nodeId),
+            );
+
+            throw new ErrorWithContext(
+                { unresolvableTypeReferenceNodes },
+                "Failed to resolve type references",
+            );
+        }
+
+        // Separate out the successes and return a map
+        const successes = resolutions.filter(
+            (pair): pair is [string, string] => pair[1] !== undefined,
+        );
+
+        this.#lookup = fromPairs(successes);
     }
 
-    // Separate out the successes and return a map
-    const successes = resolutions.filter(
-        (pair): pair is [string, string] => pair[1] !== undefined,
-    );
+    lookup(typeReferenceNodeId: string): string {
+        const resolution = this.#lookup[typeReferenceNodeId];
 
-    return fromPairs(successes);
-};
+        if (resolution === undefined) {
+            throw new ErrorWithContext(
+                { typeReferenceNodeId },
+                "Type reference unresolvable",
+            );
+        }
+
+        return resolution;
+    }
+}
